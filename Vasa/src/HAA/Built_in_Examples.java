42d4b9f5272b0185752161b3905c075317d4cd42
students <students@DESKTOP-N2HRICF>
2025-08-11 23:56:35 -0700

    Commit

---
+++ Vasa/src/HAA/Built_in_Examples.java
@@ -0,0 +1,15 @@
+package HAA;
+
+public class Built_in_Examples {
+	public static void main(String[] args) {
+		String greeting="Hello world";
+		System.out.println("Original String -"+greeting);
+		System.out.println("Upper case -"+greeting.toUpperCase());
+		System.out.println("substring -"+greeting.substring(6));
+		
+		int max=Math.max(10,20);
+		double sqrt=Math.sqrt(25.0);
+		System.out.println("max number "+ max);
+		System.out.println("sqrt "+sqrt);
+	}
+}
